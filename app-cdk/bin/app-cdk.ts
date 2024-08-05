51d230e280a9a41727f1b9e59e35bfd15cb196ec
Ariel Marihuen <eyeshield402@MacBook-Pro-de-Ariel.local>
2024-08-05 00:26:42 -0400

    first commit

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -3,10 +3,15 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AppCdkStack } from '../lib/app-cdk-stack';
 import { PipelineCdkStack } from '../lib/pipeline-cdk-stack';
+import { EcrCdkStack } from '../lib/ecr-cdk-stack';
 
 const app = new cdk.App();
 const testCdkStack = new AppCdkStack(app, 'test', {});
-const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {});
+
+const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
+const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {
+  ecrRepository: ecrCdkStack.repository,
+});
 
 new AppCdkStack(app, 'AppCdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
