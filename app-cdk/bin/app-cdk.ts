c466cfb160258f605eac9706a4f4b1bb739568e8
Ariel Marihuen <eyeshield402@MacBook-Pro-de-Ariel.local>
2024-08-04 23:52:24 -0400

    first commit

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -2,8 +2,12 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AppCdkStack } from '../lib/app-cdk-stack';
+import { PipelineCdkStack } from '../lib/pipeline-cdk-stack';
 
 const app = new cdk.App();
+const testCdkStack = new AppCdkStack(app, 'test', {});
+const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {});
+
 new AppCdkStack(app, 'AppCdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
