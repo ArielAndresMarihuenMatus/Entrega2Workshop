25d8dae394b6c663000580c8226e5b4691bee2df
Ariel Marihuen <eyeshield402@MacBook-Pro-de-Ariel.local>
2024-08-04 22:02:29 -0400

    first commit

---
+++ app/vite.config.ts
@@ -0,0 +1,18 @@
+/// <reference types='vite/client' />
+/// <reference types='vitest' />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: 'app/',
+  server: {
+    port: 8081
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  }
+})
