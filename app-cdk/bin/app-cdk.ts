2255853a7ecb7aae15beb502fa3b384fede1b50e
Ariel Marihuen <eyeshield402@MacBook-Pro-de-Ariel.local>
2024-08-10 15:21:03 -0400

    Added Prod delivery

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -6,6 +6,7 @@ import { PipelineCdkStack } from '../lib/pipeline-cdk-stack';
 import { EcrCdkStack } from '../lib/ecr-cdk-stack';
 
 const app = new cdk.App();
+
 const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
 
 const testCdkStack = new AppCdkStack(app, 'test', {
